c1bb9bf4bc80f11d63333f3ba990e40203dac5ff
Brice Pirioua <brice@Air-de-Brice.lan>
2024-11-13 00:59:23 +0100

    Updates

---
+++ static/js/952.1e66965a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdefi_bureau=self.webpackChunkdefi_bureau||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.1e66965a.chunk.js.map
--- static/js/952.81b08acd.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkdefi_bureau=self.webpackChunkdefi_bureau||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=952.81b08acd.chunk.js.map
